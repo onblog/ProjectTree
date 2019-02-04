73b4a0ca9a80ab67f7724824f222403e11aec760
yueshutong <yster@foxmail.com>
2019-02-04 23:42:00 +0800

    解决Null异常

--- src/main/java/cn/yueshutong/springprojecttree/core/ProjectTree.java
+++ src/main/java/cn/yueshutong/springprojecttree/core/ProjectTree.java
@@ -1,5 +1,7 @@
 package cn.yueshutong.springprojecttree.core;
 
+import java.util.Optional;
+
 /**
  * 方便记忆的入口方法
  * Create by yster@foxmail.com 2019/2/3 0003 12:28
@@ -8,7 +10,9 @@ public class ProjectTree {
 
     public static void make(String[] basePackage,String[] exclude) {
         ByteCodeAdvice byteCodeAdvice = new ByteCodeAdvice();
-        byteCodeAdvice.advice(basePackage,exclude);
+        Optional<String[]> basePackage1 = Optional.ofNullable(basePackage);
+        Optional<String[]> exclude1 = Optional.ofNullable(exclude);
+        byteCodeAdvice.advice(basePackage1.orElse(new String[0]),exclude1.orElse(new String[0]));
     }
 
 }
